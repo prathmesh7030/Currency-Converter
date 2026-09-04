e8fd517b213f9608703636a056b75479672a9e90
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-09-04 22:01:19 +0530

    Populate options

--- logic.js
+++ logic.js
@@ -1,2 +1,20 @@
 const BASE_URL =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+  const dropdowns = document.querySelector(".dropdown select")
+
+ 
+
+  for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+}
