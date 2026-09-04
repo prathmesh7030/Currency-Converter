d3eb0a26bcacf3b2763b5713163cd6b460a798f3
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-09-04 22:34:28 +0530

    Update exchange rate

--- logic.js
+++ logic.js
@@ -2,6 +2,10 @@ const BASE_URL =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
   const dropdowns = document.querySelectorAll(".dropdown select")
+  const btn = document.querySelector("form button");
+  const fromCurr = document.querySelector(".from select");
+  const toCurr = document.querySelector(".to select");
+  const msg = document.querySelector(".msg");
  
 
   for (let select of dropdowns) {
@@ -28,4 +32,20 @@ const updateFlag = (element) => {
   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
   let img = element.parentElement.querySelector("img");
   img.src = newSrc;
+};
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[toCurr.value.toLowerCase()];
+
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
 };
